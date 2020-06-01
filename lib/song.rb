f1ce9200c683960a211600c5a8cb7b397e8377e4
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-01 01:05:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,11 +16,9 @@ class Song
   end
 
   def artist_name=(name)
-   if (self.artist.nil?)
-     self.artist = Artist.new(name)
-   else
-     self.artist.name = name
-   end
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
  end
 
  def self.new_by_filename(filename)
